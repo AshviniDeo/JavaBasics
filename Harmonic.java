ccf442d8b8b41d5538e9401043bf5e2ba4e959f1
Ashvini Deokate <89019792+AshviniDeo@users.noreply.github.com>
2021-09-13 19:22:31 +0530

    Harmonic Programing

---
+++ Harmonic.java
@@ -0,0 +1,14 @@
+package assignment5.four;
+
+import java.util.Scanner;
+
+public class Harmonic {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter Number for Harmonic Series");
+		int number = sc.nextInt();
+		
+	}
+
+}
